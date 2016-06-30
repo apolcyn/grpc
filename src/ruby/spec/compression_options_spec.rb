b189ac08481403ccb9445acb6507f7c49d7da760
Alexander Polcyn <apolcyn@google.com>
2016-06-29 18:40:41 -0700

    some unit tests passing for options wrapper

--- src/ruby/spec/compression_options_spec.rb
+++ src/ruby/spec/compression_options_spec.rb
@@ -29,138 +29,98 @@
 
 require 'grpc'
 
-describe GRPC::Core::CompressionAlgorithms do
-  before(:each) do
-    @known_algorithms = {
-      NONE: 0,
-      DEFLATE: 1,
-      GZIP: 2,
-    }
-  end
-
-  it 'should have constants for all known compression algorithms' do
-    m = GRPC::Core::CompressionAlgorithms
-    syms_and_codes = m.constants.collect { |c| [c, m.const_get(c)] }
-    expect(Hash[syms_and_codes]).to eq(@known_algorithms)
-  end
-end
-
-describe GRPC::Core::CompressionLevels do
-  before(:each) do
-    @known_levels = {
-      NONE: 0,
-      LOW: 1,
-      MEDIUM: 2,
-      HIGH: 3,
-    }
-  end
-
-  it 'should have constants for all known compression levels' do
-    m = GRPC::Core::CompressionLevels
-    syms_and_codes = m.constants.collect { |c| [c, m.const_get(c)] }
-    expect(Hash[syms_and_codes]).to eq(@known_algorithms)
-  end
-end
-
 describe GRPC::Core::CompressionOptions do
+  GZIP = 0x4
+  DEFLATE = 0x2
+  NONE = 0x1
+
   before(:example) do
     @compression_options = GRPC::Core::CompressionOptions.new
   end
 
-  it 'should provide the correct bit set for the enabled algorithms' do
-    GZIP = 0x2
-    DEFLATE = 0x1
-    NONE = 0x1
+  describe '#enable_algorithms' do
+    it 'should pass correct internal values to #internal_enable_algorithms' do
+      expect(@compression_options).to receive(:enable_algorithm_internal)
+        .with(GZIP)
+      @compression_options.enable_algorithms(:gzip)
+    end
 
+    it 'should pass the correct internal values with multiple parameters' do
+      expect(@compression_options).to receive(:enable_algorithm_internal)
+        .with(GZIP)
+      expect(@compression_options).to receive(:enable_algorithm_internal)
+        .with(DEFLATE)
+      @compression_options.enable_algorithms(:gzip, :deflate)
+    end
+  end
+
+  describe '#disable_algorithms' do
+    it 'should pass correct internal values to #internal_disable_algorithms' do
+      expect(@compression_options).to receive(:disable_algorithm_internal)
+        .with(GZIP)
+      @compression_options.enable_algorithms(:gzip)
+    end
+
+    it 'should work with more than one parameter' do
+      expect(@compression_options).to receive(:disable_algorithm_internal)
+        .with(GZIP)
+      expect(@compression_options).to receive(:disable_algorithm_internal)
+        .with(DEFLATE)
+      @compression_options.disable_algorithms(:gzip, :deflate)
+    end
+  end
+
+  it 'should provide the correct bit set for the enabled algorithms' do
     @compression_options.enable_algorithms(:gzip, :deflate)
-    expect(@compression_options.enabled_algorithms_bitset).to eql(GZIP | DEFLATE | NONE)
+    expect(@compression_options.enabled_algorithms_bitset).to eql(
+      GZIP | DEFLATE | NONE)
 
     @compression_options.disable_algorithms(:gzip)
-    expect(@compression_options.enabled_algorithms_bitset).to eql(GZIP | NONE)
-    
+    expect(@compression_options.enabled_algorithms_bitset).to eql(
+      DEFLATE | NONE)
+
     @compression_options.disable_algorithms(:deflate)
     expect(@compression_options.enabled_algorithms_bitset).to eql(NONE)
-    
+
     @compression_options.enable_algorithms(:gzip, :deflate)
-    expect(@compression_options.enabled_algorithms_bitset).to eql(GZIP | DEFLATE | NONE)
+    expect(@compression_options.enabled_algorithms_bitset).to eql(
+      GZIP | DEFLATE | NONE)
   end
 
   it 'should be able to set the default algorithm' do
-	  [:gzip => 0, :deflate => 1, :identity => 2].each_pair do |algorithm_name, internal_value|
-	  @compression_options.default_algorithm = algorithm_name
-	  expect(@compression_options.default_algorithm_internal_value).to eql(internal_value)
-	  end
+    @compression_options.default_algorithm = :gzip
+    expect(@compression_options.default_algorithm_internal_value).to eql(2)
+
+    @compression_options.default_algorithm = :deflate
+    expect(@compression_options.default_algorithm_internal_value).to eql(1)
+
+    @compression_options.default_algorithm = :identity
+    expect(@compression_options.default_algorithm_internal_value).to eql(0)
   end
 
   it 'should be able to set the default level' do
-	  [:none => 0, :low => 1, :medium => 2, :high => 3].each_pair do |name, internal_value|
-	  @compression_options.default_level = level_name
-	  expect(@compression_options.default_level_internal_value).to eql(internal_value)
-	  end
+    @compression_options.default_level = :none
+    expect(@compression_options.default_level_internal_value).to eql(0)
+
+    @compression_options.default_level = :low
+    expect(@compression_options.default_level_internal_value).to eql(1)
+
+    @compression_options.default_level = :medium
+    expect(@compression_options.default_level_internal_value).to eql(2)
+
+    @compression_options.default_level = :high
+    expect(@compression_options.default_level_internal_value).to eql(3)
   end
 
-  describe '#new' do
+  describe 'new' do
     it 'doesnt throw an error and initializes wrapped value' do
       expect { GRPC::Core::CompressionOptions.new }.to_not raise_error
     end
 
     it 'starts out with no compression enabled' do
-	    expect(@compression_options.enabled_algorithms_bitset).to eql(0x1)
-	    expect(@compression_options.default_algorithm_internal_value).to eql(0)
-	    expect(@compression_options.default_level_internal_value).to eql(1)
-    end
-  end
-
-  describe '#enable_algorithms' do
-    it 'works with zero parameters' do
-      expect { @compression_options.enable_algorithms }.to_not raise_error
-    end
-
-    it 'works with strings or symbols' do
-      algorithm = GRPC::Core::CompressionOptions::COMPRESSION_ALGORITHMS.keys.last
-
-      expect { @compression_options.disable_algorithms(algorithm) }.to_not raise_error
-      expect { @compression_options.disable_algorithms(algorithm.downcase) }.to_not raise_error
-      expect { @compression_options.disable_algorithms(algorithm.to_s) }.to_not raise_error
-    end
-
-    it 'works with multiple parameters' do
-      algorithms = GRPC::Core::CompressionAlgorithms::COMPRESSION_ALGORITHMS.keys
-      expect { @compression_options.disable_algorithms(algorithms) }.to_not raise_error
-    end
-  end
-
-  #TODO make these examples shared
-  describe '#disable_algorithms' do
-    it 'works with zero parameters' do
-      expect { @compression_options.enable_algorithms }.to_not raise_error
-    end
-
-    it 'works with string or symbols' do
-      algorithm = GRPC::Core::CompressionOptions::COMPRESSION_ALGORITHMS.keys.last
-      expect { @compression_options.disable_algorithms(algorithm.downcase) }.to_not raise_error
-      expect { @compression_options.disable_algorithms(algorithm.to_s) }.to_not raise_error
-    end
-
-    it 'works with multiple parameters' do
-      algorithms = GRPC::Core::CompressionAlgorithms::COMPRESSION_ALGORITHMS.keys
-      expect { @compression_options.disable_algorithms(algorithms) }.to_not raise_error
-    end
-  end
-
-  describe '#default_algorithm= and #default_algorithm' do
-    it 'can be passed a string or a symbol' do
-      algorithm = GRPC::Core::CompressionOptions::COMPRESSION_ALGORITHMS.keys.last
-      expect { @compression_options.default_algorithm = algorithm }.to_not raise_error
-      expect(@compression_options.default_algorithm).to eql(algorithm)
-    end
-  end
-
-  describe '#default_level= and #default_level' do
-    it 'can be assigned a known level' do
-      level = GRPC::Core::CompressionLevels.constants.last
-      expect { @compression_options.default_level = level }.to_not raise_error
-      expect(@compression_options.default_level).to eql(level)
+      expect(@compression_options.enabled_algorithms_bitset).to eql(0x7)
+      expect(@compression_options.default_algorithm_internal_value).to eql(0)
+      expect(@compression_options.default_level_internal_value).to eql(0)
     end
   end
 end
