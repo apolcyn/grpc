9d2e6fd7c0136d0082df7b01061be288e318d780
Alex Polcyn <apolcyn@google.com>
2016-07-01 01:46:51 -0700

    some unit tests are failing

--- src/ruby/lib/grpc/core/compression_options.rb
+++ src/ruby/lib/grpc/core/compression_options.rb
@@ -33,21 +33,44 @@ require_relative '../grpc'
 module GRPC
   module Core
     # Wrapper for grpc_compression_options in core
+    # This class is defined as a C extension but is reopened here
+    # to add the initialization logic.
+    #
+    # This class wraps a GRPC core compression options.
+    #
+    # It can be used to create a channel argument key-value hash
+    # with keys and values that correspond to the compression settings
+    # provided here.
+    #
+    # call-seq:
+    #   options = CompressionOptions.new(
+    #     default_level: low,
+    #     disabled_algorithms: [:gzip])
+    #
+    # channel_args = Hash.new[...]
+    # channel_args_with_args_for_compression_settings = channel_args.merge(options)
     class CompressionOptions
-      ALGS = { dentity: 0, deflate: 2, gzip: 3 }
-
-      def enable_algorithms(*algorithms)
-        algorithms.each do |algorithm|
-          internal_value = ALGS[algorithm.to_sym]
-          enable_algorithm_internal(internal_value)
+      # Initializes a CompresionOptions instance
+      # Starts out with all available compression algorithms enabled by default
+      #
+      # @param default_level [String | Symbol] one of nil, 'none', 'low', 'medium', 'high'
+      # @param default_algorithm [String | Symbol] one of nil, 'identity', 'gzip', or 'deflate'
+      # @param disabled_algorithms [Array<String, Symbol>] can contain 'identity', 'gzip', or 'deflate'
+      def initialize(default_algorithm: nil,
+                     default_level: nil,
+                     disabled_algorithms: [])
+        if disabled_algorithms.include?(default_level)
+          raise ArgumentError("default algorithm #{default_algorithm} is listed as disabled")
         end
+
+
+        set_default_algorithm(default_algorithm) unless default_algorithm.nil?
+        set_default_level(default_level) unless default_level.nil?
+        disable_algorithms(*disabled_algorithms) unless disabled_algorithms.empty?
       end
 
-      def disable_algorithms(*algorithms)
-        algorithms.each do |algorithm|
-          internal_value = ALGS[algorithm.to_sym]
-          disable_algorithm_internal(internal_value)
-        end
+      def to_s
+        to_hash.to_s
       end
     end
   end
