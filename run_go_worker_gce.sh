0146d0b419ea2256b05fbf5d7d3a2c72a10ea3b7
Alexander Polcyn <apolcyn@google.com>
2017-02-07 11:40:12 -0800

    and another fix up

--- run_go_worker_gce.sh
+++ run_go_worker_gce.sh
@@ -8,6 +8,6 @@ fi
 
 cd ../grpc-go && git fetch origin $1 && git checkout origin/$1 || exit 1
 
-cd ../grpc && tools/run_tests/performance_build_performance_go.sh || exit 1
+cd ../grpc && tools/run_tests/performance/build_performance_go.sh || exit 1
 
 tools/run_tests/performance/run_worker_go.sh --driver_port $2 || exit 1
