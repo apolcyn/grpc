b16d5d5d946898f593675a801783298ee7b99450
Alex Polcyn <apolcyn@google.com>
2017-05-30 22:35:55 +0000

    add some stuff

--- test/core/iomgr/resolve_address_test.c
+++ test/core/iomgr/resolve_address_test.c
@@ -1,4 +1,4 @@
-/*
+ii/*
  *
  * Copyright 2015, Google Inc.
  * All rights reserved.
@@ -36,9 +36,15 @@
 #include <grpc/support/log.h>
 #include <grpc/support/sync.h>
 #include <grpc/support/time.h>
+
 #include "src/core/lib/iomgr/executor.h"
 #include "src/core/lib/iomgr/iomgr.h"
 #include "test/core/util/test_config.h"
+#include "src/core/lib/support/string.h"
+#include "src/core/lib/support/env.h"
+
+extern void grpc_resolver_dns_ares_init();
+extern void grpc_resolver_dns_ares_shutdown();
 
 static gpr_timespec test_deadline(void) {
   return grpc_timeout_seconds_to_deadline(100);
@@ -122,162 +128,190 @@ static void must_succeed(grpc_exec_ctx *exec_ctx, void *argsp,
   gpr_mu_unlock(args->mu);
 }
 
-static void must_fail(grpc_exec_ctx *exec_ctx, void *argsp, grpc_error *err) {
-  args_struct *args = argsp;
-  GPR_ASSERT(err != GRPC_ERROR_NONE);
-  gpr_atm_rel_store(&args->done_atm, 1);
-  gpr_mu_lock(args->mu);
-  GRPC_LOG_IF_ERROR("pollset_kick", grpc_pollset_kick(args->pollset, NULL));
-  gpr_mu_unlock(args->mu);
-}
-
-static void test_localhost(void) {
-  grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
-  args_struct args;
-  args_init(&exec_ctx, &args);
-  grpc_resolve_address(
-      &exec_ctx, "localhost:1", NULL, args.pollset_set,
-      grpc_closure_create(must_succeed, &args, grpc_schedule_on_exec_ctx),
-      &args.addrs);
-  grpc_exec_ctx_flush(&exec_ctx);
-  poll_pollset_until_request_done(&args);
-  args_finish(&exec_ctx, &args);
-  grpc_exec_ctx_finish(&exec_ctx);
-}
-
-static void test_default_port(void) {
-  grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
-  args_struct args;
-  args_init(&exec_ctx, &args);
-  grpc_resolve_address(
-      &exec_ctx, "localhost", "1", args.pollset_set,
-      grpc_closure_create(must_succeed, &args, grpc_schedule_on_exec_ctx),
-      &args.addrs);
-  grpc_exec_ctx_flush(&exec_ctx);
-  poll_pollset_until_request_done(&args);
-  args_finish(&exec_ctx, &args);
-  grpc_exec_ctx_finish(&exec_ctx);
-}
-
-static void test_non_numeric_default_port(void) {
-  grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
-  args_struct args;
-  args_init(&exec_ctx, &args);
-  grpc_resolve_address(
-      &exec_ctx, "localhost", "https", args.pollset_set,
-      grpc_closure_create(must_succeed, &args, grpc_schedule_on_exec_ctx),
-      &args.addrs);
-  grpc_exec_ctx_flush(&exec_ctx);
-  poll_pollset_until_request_done(&args);
-  args_finish(&exec_ctx, &args);
-  grpc_exec_ctx_finish(&exec_ctx);
-}
-
-static void test_missing_default_port(void) {
-  grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
-  args_struct args;
-  args_init(&exec_ctx, &args);
-  grpc_resolve_address(
-      &exec_ctx, "localhost", NULL, args.pollset_set,
-      grpc_closure_create(must_fail, &args, grpc_schedule_on_exec_ctx),
-      &args.addrs);
-  grpc_exec_ctx_flush(&exec_ctx);
-  poll_pollset_until_request_done(&args);
-  args_finish(&exec_ctx, &args);
-  grpc_exec_ctx_finish(&exec_ctx);
-}
-
-static void test_ipv6_with_port(void) {
-  grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
-  args_struct args;
-  args_init(&exec_ctx, &args);
-  grpc_resolve_address(
-      &exec_ctx, "[2001:db8::1]:1", NULL, args.pollset_set,
-      grpc_closure_create(must_succeed, &args, grpc_schedule_on_exec_ctx),
-      &args.addrs);
-  grpc_exec_ctx_flush(&exec_ctx);
-  poll_pollset_until_request_done(&args);
-  args_finish(&exec_ctx, &args);
-  grpc_exec_ctx_finish(&exec_ctx);
-}
-
-static void test_ipv6_without_port(void) {
-  const char *const kCases[] = {
-      "2001:db8::1", "2001:db8::1.2.3.4", "[2001:db8::1]",
-  };
-  unsigned i;
-  for (i = 0; i < sizeof(kCases) / sizeof(*kCases); i++) {
-    grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
-    args_struct args;
-    args_init(&exec_ctx, &args);
-    grpc_resolve_address(
-        &exec_ctx, kCases[i], "80", args.pollset_set,
-        grpc_closure_create(must_succeed, &args, grpc_schedule_on_exec_ctx),
-        &args.addrs);
-    grpc_exec_ctx_flush(&exec_ctx);
-    poll_pollset_until_request_done(&args);
-    args_finish(&exec_ctx, &args);
-    grpc_exec_ctx_finish(&exec_ctx);
+//static void must_fail(grpc_exec_ctx *exec_ctx, void *argsp, grpc_error *err) {
+//  args_struct *args = argsp;
+//  GPR_ASSERT(err != GRPC_ERROR_NONE);
+//  gpr_atm_rel_store(&args->done_atm, 1);
+//  gpr_mu_lock(args->mu);
+//  GRPC_LOG_IF_ERROR("pollset_kick", grpc_pollset_kick(args->pollset, NULL));
+//  gpr_mu_unlock(args->mu);
+//}
+//
+//static void test_localhost(void) {
+//  grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
+//  args_struct args;
+//  args_init(&exec_ctx, &args);
+//  grpc_resolve_address(
+//      &exec_ctx, "localhost:1", NULL, args.pollset_set,
+//      grpc_closure_create(must_succeed, &args, grpc_schedule_on_exec_ctx),
+//      &args.addrs);
+//  grpc_exec_ctx_flush(&exec_ctx);
+//  poll_pollset_until_request_done(&args);
+//  args_finish(&exec_ctx, &args);
+//  grpc_exec_ctx_finish(&exec_ctx);
+//}
+//
+static void test_resolves_srv(void) {
+  if (gpr_stricmp("ares", gpr_getenv("GRPC_DNS_RESOLVER")) != 0) {
+    gpr_log(GPR_INFO, "skipping test");
+    return;
   }
-}
-
-static void test_invalid_ip_addresses(void) {
-  const char *const kCases[] = {
-      "293.283.1238.3:1", "[2001:db8::11111]:1",
-  };
-  unsigned i;
-  for (i = 0; i < sizeof(kCases) / sizeof(*kCases); i++) {
-    grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
-    args_struct args;
-    args_init(&exec_ctx, &args);
-    grpc_resolve_address(
-        &exec_ctx, kCases[i], NULL, args.pollset_set,
-        grpc_closure_create(must_fail, &args, grpc_schedule_on_exec_ctx),
-        &args.addrs);
-    grpc_exec_ctx_flush(&exec_ctx);
-    poll_pollset_until_request_done(&args);
-    args_finish(&exec_ctx, &args);
-    grpc_exec_ctx_finish(&exec_ctx);
+  char *resolve_srv = gpr_getenv("GRPC_RESOLVE_SRV");
+  if (!resolve_srv || gpr_stricmp("", resolve_srv) == 0) {
+    gpr_log(GPR_INFO, "skipping test");
   }
+  gpr_log(GPR_INFO, "start resolves srv test");
+  grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
+  args_struct args;
+  args_init(&exec_ctx, &args);
+  grpc_resolve_address(
+      &exec_ctx, /* _grpclb._tcp. */ /* "mylbtest.test.apolcyntest" */ "mytestlb.test.apolcyntest", NULL, args.pollset_set,
+      grpc_closure_create(must_succeed, &args, grpc_schedule_on_exec_ctx),
+      &args.addrs);
+  grpc_exec_ctx_flush(&exec_ctx);
+  poll_pollset_until_request_done(&args);
+  args_finish(&exec_ctx, &args);
+  grpc_exec_ctx_finish(&exec_ctx);
+  gpr_log(GPR_INFO, "end resolves srv test");
 }
 
-static void test_unparseable_hostports(void) {
-  const char *const kCases[] = {
-      "[", "[::1", "[::1]bad", "[1.2.3.4]", "[localhost]", "[localhost]:1",
-  };
-  unsigned i;
-  for (i = 0; i < sizeof(kCases) / sizeof(*kCases); i++) {
-    grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
-    args_struct args;
-    args_init(&exec_ctx, &args);
-    grpc_resolve_address(
-        &exec_ctx, kCases[i], "1", args.pollset_set,
-        grpc_closure_create(must_fail, &args, grpc_schedule_on_exec_ctx),
-        &args.addrs);
-    grpc_exec_ctx_flush(&exec_ctx);
-    poll_pollset_until_request_done(&args);
-    args_finish(&exec_ctx, &args);
-    grpc_exec_ctx_finish(&exec_ctx);
-  }
-}
-
+//static void test_default_port(void) {
+//  grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
+//  args_struct args;
+//  args_init(&exec_ctx, &args);
+//  grpc_resolve_address(
+//      &exec_ctx, "localhost", "1", args.pollset_set,
+//      grpc_closure_create(must_succeed, &args, grpc_schedule_on_exec_ctx),
+//      &args.addrs);
+//  grpc_exec_ctx_flush(&exec_ctx);
+//  poll_pollset_until_request_done(&args);
+//  args_finish(&exec_ctx, &args);
+//  grpc_exec_ctx_finish(&exec_ctx);
+//}
+//
+//static void test_non_numeric_default_port(void) {
+//  grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
+//  args_struct args;
+//  args_init(&exec_ctx, &args);
+//  grpc_resolve_address(
+//      &exec_ctx, "localhost", "https", args.pollset_set,
+//      grpc_closure_create(must_succeed, &args, grpc_schedule_on_exec_ctx),
+//      &args.addrs);
+//  grpc_exec_ctx_flush(&exec_ctx);
+//  poll_pollset_until_request_done(&args);
+//  args_finish(&exec_ctx, &args);
+//  grpc_exec_ctx_finish(&exec_ctx);
+//}
+//
+//static void test_missing_default_port(void) {
+//  grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
+//  args_struct args;
+//  args_init(&exec_ctx, &args);
+//  grpc_resolve_address(
+//      &exec_ctx, "localhost", NULL, args.pollset_set,
+//      grpc_closure_create(must_fail, &args, grpc_schedule_on_exec_ctx),
+//      &args.addrs);
+//  grpc_exec_ctx_flush(&exec_ctx);
+//  poll_pollset_until_request_done(&args);
+//  args_finish(&exec_ctx, &args);
+//  grpc_exec_ctx_finish(&exec_ctx);
+//}
+//
+//static void test_ipv6_with_port(void) {
+//  grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
+//  args_struct args;
+//  args_init(&exec_ctx, &args);
+//  grpc_resolve_address(
+//      &exec_ctx, "[2001:db8::1]:1", NULL, args.pollset_set,
+//      grpc_closure_create(must_succeed, &args, grpc_schedule_on_exec_ctx),
+//      &args.addrs);
+//  grpc_exec_ctx_flush(&exec_ctx);
+//  poll_pollset_until_request_done(&args);
+//  args_finish(&exec_ctx, &args);
+//  grpc_exec_ctx_finish(&exec_ctx);
+//}
+//
+//static void test_ipv6_without_port(void) {
+//  const char *const kCases[] = {
+//      "2001:db8::1", "2001:db8::1.2.3.4", "[2001:db8::1]",
+//  };
+//  unsigned i;
+//  for (i = 0; i < sizeof(kCases) / sizeof(*kCases); i++) {
+//    grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
+//    args_struct args;
+//    args_init(&exec_ctx, &args);
+//    grpc_resolve_address(
+//        &exec_ctx, kCases[i], "80", args.pollset_set,
+//        grpc_closure_create(must_succeed, &args, grpc_schedule_on_exec_ctx),
+//        &args.addrs);
+//    grpc_exec_ctx_flush(&exec_ctx);
+//    poll_pollset_until_request_done(&args);
+//    args_finish(&exec_ctx, &args);
+//    grpc_exec_ctx_finish(&exec_ctx);
+//  }
+//}
+//
+//static void test_invalid_ip_addresses(void) {
+//  const char *const kCases[] = {
+//      "293.283.1238.3:1", "[2001:db8::11111]:1",
+//  };
+//  unsigned i;
+//  for (i = 0; i < sizeof(kCases) / sizeof(*kCases); i++) {
+//    grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
+//    args_struct args;
+//    args_init(&exec_ctx, &args);
+//    grpc_resolve_address(
+//        &exec_ctx, kCases[i], NULL, args.pollset_set,
+//        grpc_closure_create(must_fail, &args, grpc_schedule_on_exec_ctx),
+//        &args.addrs);
+//    grpc_exec_ctx_flush(&exec_ctx);
+//    poll_pollset_until_request_done(&args);
+//    args_finish(&exec_ctx, &args);
+//    grpc_exec_ctx_finish(&exec_ctx);
+//  }
+//}
+//
+//static void test_unparseable_hostports(void) {
+//  const char *const kCases[] = {
+//      "[", "[::1", "[::1]bad", "[1.2.3.4]", "[localhost]", "[localhost]:1",
+//  };
+//  unsigned i;
+//  for (i = 0; i < sizeof(kCases) / sizeof(*kCases); i++) {
+//    grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
+//    args_struct args;
+//    args_init(&exec_ctx, &args);
+//    grpc_resolve_address(
+//        &exec_ctx, kCases[i], "1", args.pollset_set,
+//        grpc_closure_create(must_fail, &args, grpc_schedule_on_exec_ctx),
+//        &args.addrs);
+//    grpc_exec_ctx_flush(&exec_ctx);
+//    poll_pollset_until_request_done(&args);
+//    args_finish(&exec_ctx, &args);
+//    grpc_exec_ctx_finish(&exec_ctx);
+//  }
+//}
+//
 int main(int argc, char **argv) {
   grpc_test_init(argc, argv);
   grpc_executor_init();
   grpc_iomgr_init();
   grpc_iomgr_start();
-  test_localhost();
-  test_default_port();
-  test_non_numeric_default_port();
-  test_missing_default_port();
-  test_ipv6_with_port();
-  test_ipv6_without_port();
-  test_invalid_ip_addresses();
-  test_unparseable_hostports();
+  grpc_resolver_dns_ares_init();
+  //test_localhost();
+  //test_default_port();
+  //test_non_numeric_default_port();
+  //test_missing_default_port();
+  //test_ipv6_with_port();
+  //test_ipv6_without_port();
+  //test_invalid_ip_addresses();
+  //test_unparseable_hostports();
+  gpr_setenv("GRPC_DNS_RESOLVER", "ares");
+  test_resolves_srv();
   {
     grpc_exec_ctx exec_ctx = GRPC_EXEC_CTX_INIT;
     grpc_executor_shutdown(&exec_ctx);
     grpc_iomgr_shutdown(&exec_ctx);
+    grpc_resolver_dns_ares_shutdown(&exec_ctx);
     grpc_exec_ctx_finish(&exec_ctx);
   }
   return 0;
