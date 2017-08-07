42aea3db866b7f6b77463ff3e37272ba564f0f55
Alexander Polcyn <apolcyn@google.com>
2017-08-07 10:39:44 -0700

    add breadth forking tests

---
+++ src/ruby/end2end/breadth_forking_client.rb
@@ -0,0 +1,81 @@
+#!/usr/bin/env ruby
+
+# Copyright 2015 gRPC authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Prompted by and minimal repro of https://github.com/grpc/grpc/issues/10658
+
+require_relative './end2end_common'
+
+def main
+  server_port = ''
+  OptionParser.new do |opts|
+    opts.on('--client_control_port=P', String) do
+      STDERR.puts 'client control port not used'
+    end
+    opts.on('--server_port=P', String) do |p|
+      server_port = p
+    end
+  end.parse!
+
+  client_opts = {
+    channel_args: {
+      GRPC::Core::Channel::SSL_TARGET => 'foo.test.google.fr'
+    }
+  }
+
+  stub = Echo::EchoServer::Stub.new("localhost:#{server_port}",
+                                    create_channel_creds,
+                                    **client_opts)
+  p "attempt RPC from parent"
+  stub.echo_verify_call_creds(Echo::EchoRequest.new(request: 'hello'))
+  p "finished RPC from parent"
+
+  child_processes = []
+
+  10.times do
+    GRPC::Core::ForkingContext.prefork()
+
+    child_processes << fork do
+      GRPC::Core::ForkingContext.postfork_child()
+
+      p "attempt RPC from child"
+      stub.echo_verify_call_creds(Echo::EchoRequest.new(request: 'hello'))
+      p "finished RPC from child"
+    end
+
+    GRPC::Core::ForkingContext.postfork_parent()
+  end
+
+  exit_code = 0
+
+  child_processes.each do |child_proc|
+    begin
+      Timeout.timeout(10) do
+        Process.wait(child_proc)
+      end
+    rescue Timeout::Error
+      STDERR.puts "timeout waiting for forked process #{p}"
+      Process.kill('SIGKILL', p)
+      Process.wait(p)
+      raise 'Timed out waiting for client process. ' \
+        'It likely hangs when using gRPC after loading it and then forking'
+    end
+    exit_code |= $?.to_i
+  end
+
+  fail "forked process failed: #{exit_code}" if exit_code != 0
+end
+
+main
