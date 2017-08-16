80e043336136dc700f5d28554502503c374456b2
Alexander Polcyn <apolcyn@google.com>
2017-08-15 18:01:16 -0700

    tweak

--- tools/jenkins/run_naming_end2end_with_gce_dns.sh
+++ tools/jenkins/run_naming_end2end_with_gce_dns.sh
@@ -24,5 +24,7 @@ CPP_BUILD_CONFIG=${CPP_BUILD_CONFIG:-dbg}
 cd $(dirname $0)/../..
 
 tools/run_tests/run_naming_with_gce_dns_sanity_check.py
+
+# use an empty authority in test uri's
 export GRPC_DNS_AUTHORITY_TESTING_OVERRIDE=''
 tools/run_tests/run_tests.py -l c++ -c $CPP_BUILD_CONFIG -r naming_end2end --use_docker || true
