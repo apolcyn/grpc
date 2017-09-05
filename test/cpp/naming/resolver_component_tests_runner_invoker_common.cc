fff98b5871a0205a093b58c566784aa11e08d425
Alexander Polcyn <apolcyn@google.com>
2017-09-05 00:10:40 -0700

    towarrds propogating signals to child threads

--- test/cpp/naming/resolver_component_tests_runner_invoker_common.cc
+++ test/cpp/naming/resolver_component_tests_runner_invoker_common.cc
@@ -38,6 +38,38 @@ extern "C" {
 
 using grpc::SubProcess;
 
+volatile sig_atomic_t abort = 0;
+
+namespace {
+  gpr_mu
+  std::mutex g_test_driver_mu;
+  std::condition_variable g_test_driver_cv;
+  int g_test_driver_done;
+  SubProcess *g_test_driver = nullptr;
+
+  void RunSigHandlingThread(SubProcess *test_driver, gpr_mu *test_driver_mu, gpr_cv *test_driver_cv, int *test_driver_done) {
+    for (;;) {
+      if (abort) {
+        gpr_mu_lock(test_driver_mu);
+        if (test_driver != nullptr) {
+          test_driver->Interrupt();
+        }
+        gpr_mu_unlock(test_driver_mu);
+        return;
+      }
+      gpr_mu_lock(test_driver_mu);
+      if (*test_driver_done) {
+        gpr_mu_unlock(test_driver_mu);
+        return;
+      }
+      gpr_cv_wait(test_driver_mu, test_driver_cv, gpr_time_add(gpr_now(GPR_CLOCK_REALTIME),
+                                                               gpr_time_from_seconds(1, GPR_TIMESPAN)));
+      gpr_mu_unlock(test_driver_mu);
+      gpr_mu_lock(test_driver_mu);
+    }
+  }
+}
+
 namespace grpc {
 
 namespace testing {
@@ -48,11 +80,19 @@ void InvokeResolverComponentTestsRunner(std::string test_runner_bin_path,
                                         std::string records_config_path) {
   grpc_init();
   int test_dns_server_port = grpc_pick_unused_port_or_die();
+
   SubProcess *test_driver = new SubProcess(
       {test_runner_bin_path, "--test_bin_path=" + test_bin_path,
        "--dns_server_bin_path=" + dns_server_bin_path,
        "--records_config_path=" + records_config_path,
        "--test_dns_server_port=" + std::to_string(test_dns_server_port)});
+  gpr_mu test_driver_mu;
+  gpr_mu_init(&test_driver_mu);
+  gpr_cv test_driver_cv;
+  gpr_cv_init(&test_driver_cv);
+  int test_driver_done = 0;
+  std::thread sig_handling_thread = SigHandlingThread(test_driver, &test_driver_mu, &test_driver_cv, &test_driver_done);
+
   int status = test_driver->Join();
   if (WIFEXITED(status)) {
     if (WEXITSTATUS(status)) {
@@ -73,6 +113,8 @@ void InvokeResolverComponentTestsRunner(std::string test_runner_bin_path,
     abort();
   }
   delete test_driver;
+  gpr_mu_destroy(&test_driver_mu);
+  gpr_cv_destroy(&test_driver_);
   grpc_shutdown();
 }
 
