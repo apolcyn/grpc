eb17a4dffe03e46e729e982381223820aa699abb
apolcyn <apolcyn@users.noreply.github.com>
2022-10-18 05:21:51 +0000

    Automated change: Fix sanity tests

--- tools/run_tests/run_interop_tests.py
+++ tools/run_tests/run_interop_tests.py
@@ -274,7 +274,11 @@ class GoLanguage:
         return ['go', 'run', 'server.go'] + args
 
     def global_env(self):
-      return {'GO111MODULE': 'on', 'GRPC_GO_LOG_VERBOSITY_LEVEL': '99', 'GRPC_GO_LOG_SEVERITY_LEVEL': 'INFO'}
+        return {
+            'GO111MODULE': 'on',
+            'GRPC_GO_LOG_VERBOSITY_LEVEL': '99',
+            'GRPC_GO_LOG_SEVERITY_LEVEL': 'INFO'
+        }
 
     def unimplemented_test_cases(self):
         return _SKIP_COMPRESSION + \
