3e5ca08fa67b52be08800965d1788a66bdce090f
Alexander Polcyn <apolcyn@google.com>
2022-11-15 18:02:32 +0000

    remove prepare build macos from artifact build

--- tools/internal_ci/macos/grpc_build_artifacts.sh
+++ tools/internal_ci/macos/grpc_build_artifacts.sh
@@ -21,8 +21,6 @@ source $(dirname $0)/../../../tools/internal_ci/helper_scripts/move_src_tree_and
 # change to grpc repo root
 cd $(dirname $0)/../../..
 
-source tools/internal_ci/helper_scripts/prepare_build_macos_rc
-
 echo "The grpc_build_artifacts -> grpc_build_packages -> grpc_distribtests build flow has been deprecated."
 echo "To build the packages for a grpc release, use the grpc_build_all_packages job."
 echo "(which relies on the per-language distribtests jobs to build and test the packages)."
