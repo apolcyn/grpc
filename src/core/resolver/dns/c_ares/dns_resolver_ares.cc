7b7dac71a3fb963bd050f2d504c2b7694bfcbe80
apolcyn <9566254+apolcyn@users.noreply.github.com>
2025-06-07 16:10:32 +0000

    Automated change: Fix sanity tests

--- src/core/resolver/dns/c_ares/dns_resolver_ares.cc
+++ src/core/resolver/dns/c_ares/dns_resolver_ares.cc
@@ -236,7 +236,8 @@ void AresClientChannelDNSResolver::AresRequestWrapper::OnHostnameResolved(
   if (error.ok()) {
     GRPC_TRACE_VLOG(cares_resolver, 2)
         << "(c-ares resover) resolver:" << self
-        << " OnHostnameResolved addresses_: " << (self->addresses_ != nullptr ? "non-null addresses" : "nullptr");
+        << " OnHostnameResolved addresses_: "
+        << (self->addresses_ != nullptr ? "non-null addresses" : "nullptr");
   } else {
     GRPC_TRACE_VLOG(cares_resolver, 2)
         << "(c-ares resover) resolver:" << self
